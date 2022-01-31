e4c18d105a404962fd65db71fcaadee25c23f8ec
Zandor leal <zandor.leal@instruct.com>
2022-01-31 11:23:50 -0300

    02

--- server/s3/index.ts
+++ server/s3/index.ts
@@ -3,9 +3,11 @@ import aws from "aws-sdk";
 const { S3 } = aws;
 
 const s3Client = new S3({
+  endpoint: process.env.BUCKETEER_ENDPOINT,
   accessKeyId: process.env.BUCKETEER_AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.BUCKETEER_AWS_SECRET_ACCESS_KEY,
-  region: "us-east-1",
+  region: process.env.BUCKETEER_REGION, //"us-east-1",
+
 });
 
 export default s3Client;
