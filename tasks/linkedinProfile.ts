63fa2158a2948d3160957fa9f11b40addb8e4e3a
Zandor leal <zandor.leal@instruct.com>
2022-02-02 11:48:47 -0300

    AGORA VAI 01

--- tasks/linkedinProfile.ts
+++ tasks/linkedinProfile.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 const { REDIS_URL } = process.env;
 
-// 'rediss://default:3tjfwxeHJGLgEEUt@db-redis-tor1-57664-do-user-10735511-0.b.db.ondigitalocean.com:25061'
+
 export const linkedinQueue = kue.createQueue({
     Prefix: 'linkedin', 
     redis: {createClientFactory: () => redis.createClient( process.env.REDIS_URL, 
